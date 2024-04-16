6927158a88d023656118d029ed0f855dadffcae0
Xiaoyu <always.zhang@gmail.com>
2024-04-17 02:39:39 +0800

    now it works !

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,7 +13,7 @@ import GHC.Generics
 import Data.Aeson hiding (json)
 import Data.Aeson.TH
 import Data.Aeson.Types
-
+import Data.Map
 
 data Size = Big   
           | Small Apple
@@ -25,10 +25,28 @@ data Apple = AppleA
            | Apples Apple
             deriving (Show,Eq,Ord,Read,Generic)
 
+data Color = Red 
+           | Blue
+            deriving (Show,Eq,Ord,Read,Generic)
+
+
+data Basket = Basket (Map Color Size)
+            | Dummy
+            deriving (Show,Eq,Ord,Read,Generic)
+
 
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-$(deriveJSON defaultOptions ''Size)
-$(deriveJSON defaultOptions ''Apple)
+$(concat <$> traverse (deriveJSON defaultOptions) [''Size, ''Apple, ''Basket, ''Color])
+
+instance ToJSONKey Color where
+  toJSONKey = genericToJSONKey defaultJSONKeyOptions
+
+instance FromJSONKey Color where
+  fromJSONKey = genericFromJSONKey defaultJSONKeyOptions
+
+
+-- $(deriveJSON defaultOptions ''Apple)
+-- $(deriveJSON defaultOptions ''Size)
